0a81d4107a312455b6a29f3b23a45f9442f4f7f1
Vasyl Horaichuk <vg.createam@gmail.com>
2022-11-20 19:19:19 +0200

    Виконую умову

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+
+
+https://restcountries.com/v2/{service}?fields={field},{field},{field}
+https://restcountries.com/v2/all?fields=name,capital,currencies
+// const searchParams = new URLSearchParams({
+//   _limit: 5,
+//   _sort: "name",
+// });
+
+// console.log(searchParams.toString());
+fetchCountries(name) { 
+    const url = 'https://restcountries.com/v3.1/name/';
+    const filter = '?fields=name.official,capital,currencies'
+}
+
+
+console.log(url);
+
+// Робить запит на бекенд
+
+
+// Винести її в окремий файл та зробити іменований експорт 
+fetchCountries.js 
