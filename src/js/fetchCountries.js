1887a42c696c3fe38db512699a2199387ce76fa5
Vasyl Horaichuk <vg.createam@gmail.com>
2022-11-29 19:46:37 +0200

    Виконав

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,11 @@
 
-
-export fetchCountries(name) {
-    const endpoint = 'https://restcountries.com/v3.1/name/';
-    const qweryParams = '?fields=name,capital,population,flags,languages';
-    const requestUrl = `${endpoint}${name}${qweryParams}`;
-    
-    fetch(requestUrl).then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        return response.json();
-    });
+export function fetchCountries(name) {
+  const url = 'https://restcountries.com/v3.1/name/';
+  const filter = '?fields=name,capital,population,flags,languages';
+  return fetch(`${url}${name}${filter}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
