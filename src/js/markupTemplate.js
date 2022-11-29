7d07836e7f57b66ff71135dffb7d151f1fed5db9
Vasyl Horaichuk <vg.createam@gmail.com>
2022-11-29 20:03:35 +0200

    NHN

--- src/js/markupTemplate.js
+++ src/js/markupTemplate.js
@@ -1,4 +1,4 @@
-export function countryCardTeemplate({ flags, name, capital, population, languages }) {
+export function countryCardTemplate({ flags, name, capital, population, languages }) {
   return `
     <div class="country-info__container">
       <div class="country-info__wrapper">
