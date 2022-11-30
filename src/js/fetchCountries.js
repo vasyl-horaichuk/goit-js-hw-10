c69e258ee938efc2d46301c06bd73f459e3107d0
Vasy Horaichuk <93982881+vasyl-horaichuk@users.noreply.github.com>
2022-11-30 15:15:51 +0200

    vsdbv

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -8,7 +8,7 @@ export function fetchCountries(name) {
   const url = 'https://restcountries.com/v3.1/name/';
   const filter = '?fields=name,capital,population,flags,languages';
 
-  return fetch(${url}${name}${filter}).then(response => {
+  return fetch(`${url}${name}${filter}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
