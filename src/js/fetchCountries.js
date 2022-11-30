724762531e7fd4a3fbcc1b96c1e58d6dca9b0e2e
Vasy Horaichuk <93982881+vasyl-horaichuk@users.noreply.github.com>
2022-11-30 15:10:26 +0200

    Виправив

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,24 @@
+// export function fetchCountries(name) {
+//   const url = 'https://restcountries.com/v3.1/name/';
+//   const filter = '?fields=name,capital,population,flags,languages';
+//   return fetch(`${url}${name}${filter}`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+    
+//     return response.json();
+//   });
+// }
+
 export function fetchCountries(name) {
+name = name.trim();
+
+if(name.langth === 0) {
+   return [];
+}
   const url = 'https://restcountries.com/v3.1/name/';
   const filter = '?fields=name,capital,population,flags,languages';
-  return fetch(`${url}${name}${filter}`).then(response => {
+  return fetch(${url}${name}${filter}).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
