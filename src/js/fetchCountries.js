eee4e8037675c74b48c9482bf56ce3e180938312
Vasy Horaichuk <93982881+vasyl-horaichuk@users.noreply.github.com>
2022-11-30 18:57:07 +0200

    виправив

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,13 @@
 
 export function fetchCountries(name) {
-  name = name.trim();
-  if(name.length == 0) {
-     return [];
-  }
   const url = 'https://restcountries.com/v3.1/name/';
   const filter = '?fields=name,capital,population,flags,languages';
 
   return fetch(`${url}${name}${filter}`).then(response => {
     if (!response.ok) {
+       if(response.status === 404) {
+           return [];
+        }
       throw new Error(response.status);
     }
     
